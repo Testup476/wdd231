2e1f6583cc78d7960037f3f0e800a64b327b9cae
Testup476 <Kandajeanmarc@gmail.com>
2025-07-16 23:56:01 +0100

    Weather

---
+++ scripts/weathertemplate.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const currentTemp = document.querySelector('#current-temp');
+    const weatherIcon = document.querySelector('#weather-icon');
+    const captionDesc = document.querySelector('figcaption');
+    const apikey = `13eeceb063b83c69c194d0f010109db6`;
+    const latitude = 49.75;
+    const longitude = 6.63;
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${apikey}`;
+
+    async function apifetch() {
+        try {
+            const respons = await fetch(url);
+            if (!respons.ok) {
+                throw new respons.error('No Data to fetch', await respons.text());
+            }
+            const data = await respons.json();
+            // renderdatat(data);
+            displayResults(data);
+            console.log(data);
+
+
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+
+    function displayResults(data) {
+        captionDesc.innerHTML = data.weather[0].description;
+        currentTemp.innerHTML = `${data.main.temp}&degre;C`;
+        const iconsource = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+
+        weatherIcon.setAttribute('src', iconsource);
+        weatherIcon.setAttribute('alt', data.weather[0].icon);
+    }
+
+
+    apifetch();
+
+
+})
