97df101c6ec1cebf184390387018f916cc6a8714
Testup476 <Kandajeanmarc@gmail.com>
2025-07-27 11:43:29 +0100

    secode

--- chamber/scripts/dates.js
+++ chamber/scripts/dates.js
@@ -1,1 +1,4 @@
-
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById("currentyear").textContent = "©" + new Date().getFullYear();
+    document.getElementById("lastModified").textContent = "Last modified : " + document.lastModified;
+});
